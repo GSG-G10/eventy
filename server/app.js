5bfee5c7a4e1f1fcd4c44c683366498709f0ca35
umar <umars.salah@gmail.com>
2021-10-22 13:08:16 +0300

    Express Setup Relates #7

---
+++ server/app.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+
+const express = require('express');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+const { join } = require('path');
+
+const router = require('./routes');
+
+const app = express();
+
+const {
+  env: { PORT, NODE_ENV },
+} = process;
+
+app.disable('x-powered-by');
+
+app.set('port', PORT || 5000);
+
+app.use([compression(), cookieParser(), express.urlencoded({ extended: false }), express.json()]);
+
+app.use('/api/v1/', router);
+
+if (NODE_ENV === 'development') {
+  app.get('/', (req, res) => {
+    res.json({ message: 'Server is running' });
+  });
+}
+
+if (NODE_ENV === 'production') {
+  app.use(express.static(join(__dirname, '..', 'client', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
+  });
+}
+
+module.exports = app;
