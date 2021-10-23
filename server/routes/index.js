67b03298f74fa4d87c87695fde0f34816f83bbad
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-10-23 20:43:02 +0300

    add orgs controller to routes Relates #23

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,6 @@
-const router = require('express').Router;
+const router = require('express').Router();
+const { getOrgs } = require('../controllers');
+
+router.get('/organizations', getOrgs);
 
 module.exports = router;
