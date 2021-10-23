43cca5cbe232cff0b87329b051ad662e9dd9069b
Your Name <noorin.sa.99@gmail.com>
2021-10-24 01:18:11 +0300

    Edit ssl value Relates #22

--- server/database/connection/index.js
+++ server/database/connection/index.js
@@ -26,8 +26,7 @@ switch (NODE_ENV) {
 
 const options = {
   connectionString: dbUrl,
-  // ssl: { rejectUnauthorized: false },
-  ssl:false
+  ssl: { rejectUnauthorized: false },
 };
 
 module.exports = new Pool(options);
