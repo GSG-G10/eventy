a9358edf3b2005be6c4de9b2d3e0f892b7f45a12
Your Name <noorin.sa.99@gmail.com>
2021-10-23 17:11:37 +0300

    create a database schema for eventy Relates #19

---
+++ server/database/config/build.sql
@@ -0,0 +1,45 @@
+BEGIN;
+  DROP TABLE IF EXISTS ORGANIZATION,EVENTS,ATTENDANCE,EVENT_ATTENDANCE CASCADE;
+
+  CREATE TABLE ORGANIZATION(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(80) NOT NULL,
+      email VARCHAR(150) UNIQUE NOT NULL,
+      password VARCHAR(100) NOT NULL,
+      image TEXT NOT NULL,
+      categories TEXT
+  );
+
+  CREATE TABLE EVENTS(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+      description TEXT NOT NULL,
+      price INTEGER NOT NULL DEFAULT 0,
+      attendance INTEGER DEFAULT 0,
+      start_date DATE NOT NULL,
+      expire_date DATE NOT NULL,
+      location VARCHAR(200) NOT NULL,
+      image TEXT NOT NULL,
+      duration TIME NOT NULL,
+      details VARCHAR(250) NOT NULL,
+      organizer INTEGER REFERENCES ORGANIZATION(id),
+      category TEXT NOT NULL
+  );
+
+  CREATE TABLE ATTENDANCE(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+      email VARCHAR(150) UNIQUE NOT NULL,
+      age INTEGER NOT NULL,
+      gender VARCHAR(50) NOT NULL,
+      isSubscriber BOOLEAN NOT NULL,
+      phone INTEGER
+  );
+
+  CREATE TABLE EVENT_ATTENDANCE(
+      id SERIAL PRIMARY KEY,
+      attendance_id INTEGER REFERENCES ATTENDANCE(id),
+      event_id INTEGER REFERENCES EVENTS(id)
+  );
+
+COMMIT;
