40a63b54e0d08df120f3314e1121145424b0ec68
umar <umars.salah@gmail.com>
2021-10-23 14:00:05 +0300

    Database config Relates #8

---
+++ server/database/config/build.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+const connection = require('../connection');
+
+const dbBuilder = () => {
+  const filepath = path.join(__dirname, 'build.sql');
+  let sql = fs.readFileSync(filepath).toString();
+  sql += fs.readFileSync(path.join(__dirname, 'fakedata.sql')).toString();
+  return connection.query(sql);
+};
+
+dbBuilder();
+
+module.exports = dbBuilder;
