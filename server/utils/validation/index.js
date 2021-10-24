4586a8b0f1c870fa03335ccd453613b59cedc085
Your Name <noorin.sa.99@gmail.com>
2021-10-24 23:57:55 +0300

    Join event validation schema Relates #27

--- server/utils/validation/index.js
+++ server/utils/validation/index.js
@@ -1,7 +1,9 @@
 const loginSchema = require('./login');
 const signupValidation = require('./signup-schema-validation');
+const joinEventValidation = require('./join-event-validation');
 
 module.exports = {
   loginSchema,
   signupValidation,
+  joinEventValidation
 };
