c0cc13228f98cc88ca09d170a9ee2a73d766bb89
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-10-25 01:03:42 +0300

    add comments

--- server/controllers/auth/login/index.js
+++ server/controllers/auth/login/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-throw-literal */
 const { comparePassword } = require('../../../utils/b-crypt');
 
 const { checkAccount } = require('../../../database/queries');
@@ -7,11 +6,14 @@ const { loginSchema } = require('../../../utils/validation');
 const login = async (req, res, next) => {
   const { email, password } = req.body;
   try {
+    // input validation
     await loginSchema.validateAsync(req.body);
+    // check if user exists
     const { rows } = await checkAccount(email);
     if (!rows.length) {
       return res.status(401).json({ message: 'invalid email or password' });
     }
+    // compare passwords
     const compared = await comparePassword(password, rows[0].password);
     if (!compared) {
       return res.status(401).json({ message: 'invalid email or password' });
