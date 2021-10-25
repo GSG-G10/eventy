b4747c8269605dd5ed3bd4bcdc343641c3aecc1e
umar <umars.salah@gmail.com>
2021-10-25 17:37:19 +0300

    id param validation Relates #11

--- server/controllers/events/update-event.js
+++ server/controllers/events/update-event.js
@@ -3,13 +3,16 @@ const { updateEventQuery } = require('../../database/queries');
 module.exports = async (req, res, next) => {
   try {
     const { id } = req.params;
-    // await schema validation here
-    await updateEventQuery(id, req.body);
-    return res
-      .json({
-        status: 200,
-        message: 'Event updated successfully',
-      });
+    if (id > 0) {
+      // await schema validation here
+      await updateEventQuery(id, req.body);
+      return res
+        .json({
+          status: 200,
+          message: 'Event updated successfully',
+        });
+    }
+    return res.status(400).json({ status: 400, message: 'Bad Request' });
   } catch (err) {
     return next(err);
   }
