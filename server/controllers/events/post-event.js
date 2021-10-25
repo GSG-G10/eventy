7ccd661543055b37db997a48ca7d5def27d35346
Your Name <noorin.sa.99@gmail.com>
2021-10-26 01:59:55 +0300

    adding a validation schema Relates #29

--- server/controllers/events/post-event.js
+++ server/controllers/events/post-event.js
@@ -1,8 +1,10 @@
 const { createEventQuery } = require('../../database/queries/events');
+const { createEventValidation } = require('../../utils/validation');
 
 const createEvent = async (req, res, next) => {
   try {
     const { userId } = req;
+    await createEventValidation.validateAsync(req.body);
     await createEventQuery(userId, req.body);
     return res.json({
       status: 200,
--- server/utils/validation/index.js
+++ server/utils/validation/index.js
@@ -1,9 +1,11 @@
 const loginSchema = require('./login');
 const signupValidation = require('./signup-schema-validation');
 const joinEventValidation = require('./join-event-validation');
+const createEventValidation = require('./create-event-schema');
 
 module.exports = {
   loginSchema,
   signupValidation,
   joinEventValidation,
+  createEventValidation,
 };
