daba27a0e28ff81bd426077d3c809791d04f0a73
umar <umars.salah@gmail.com>
2021-10-26 13:51:12 +0300

    Edit schema validation Relates #11

--- server/utils/validation/update-event-validation.js
+++ server/utils/validation/update-event-validation.js
@@ -1,17 +1,17 @@
 const joi = require('joi');
 
 module.exports = joi.object({
-  id: joi.number().required(),
+  id: joi.number().positive().required(),
   name: joi.string().required(),
   description: joi.string().required(),
-  price: joi.number().required(),
-  attendance: joi.number().required(),
+  price: joi.number().positive().required(),
+  attendance: joi.number().positive().required(),
   startDate: joi.date().required(),
   expireDate: joi.date().required(),
   location: joi.string().required(),
   image: joi.string(),
   duration: joi.string().required(),
   details: joi.string().required(),
-  organizer_id: joi.number().required(),
+  organizer_id: joi.number().positive().required(),
   category: joi.string(),
 });
