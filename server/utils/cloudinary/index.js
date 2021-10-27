8614d8526f3872e6f90af5c37266333d6e6ae594
umar <umars.salah@gmail.com>
2021-10-27 19:15:53 +0300

    Use cloudinary to upload and store photos

---
+++ server/utils/cloudinary/index.js
@@ -0,0 +1,14 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadImage = async (imageEncoded) => {
+  const uploadResponse = await cloudinary.uploader.upload(imageEncoded);
+  return uploadResponse.url;
+};
+
+module.exports = { uploadImage };
