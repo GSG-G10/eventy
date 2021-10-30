53fd2dab8c6e72f46277e409b5a506b3f796c75f
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-10-30 19:06:50 +0200

    edit all queries that get events to fetch the organizer name Relates #84

--- server/database/queries/events/get-events-category.js
+++ server/database/queries/events/get-events-category.js
@@ -2,9 +2,11 @@ const connection = require('../../connection');
 
 module.exports = async (category, date) => {
   const { rows } = await connection.query(`
-    SELECT * FROM events 
-    WHERE category = $1 AND expire_date >= $2 
-    ORDER BY attendance DESC `,
+    SELECT events.*, organization.name
+    FROM events INNER JOIN organization
+    ON events.organizer_id = organization.id  
+    WHERE events.category = $1 AND expire_date >= $2 
+    ORDER BY events.attendance DESC `,
   [category, date]);
 
   return rows;
