f9383e0b1bfc33ca4a0a01f11ae113bb83932874
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-10-30 18:48:18 +0200

    edit getEventByIdQuery to fetch the organizer name Relates #84

--- server/database/queries/events/get-event-id.js
+++ server/database/queries/events/get-event-id.js
@@ -2,7 +2,10 @@ const connection = require('../../connection');
 
 module.exports = async (eventId) => {
   const { rows } = await connection.query(
-    'SELECT * FROM events WHERE id=$1', [eventId],
+    `SELECT events.*, organization.name
+     FROM events INNER JOIN organization 
+     ON events.organizer_id =organization.id 
+     WHERE events.id = 3;`, [eventId],
   );
   return rows[0];
 };
