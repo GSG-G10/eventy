53132b9ef9c6b117a35b6ff82bab969f0cb80226
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-10-31 17:24:27 +0200

    add 'AS organizer' for organizer name to distinguish between names

--- server/database/queries/events/get-top-event.js
+++ server/database/queries/events/get-top-event.js
@@ -3,7 +3,7 @@ const connection = require('../../connection');
 module.exports = async (date) => {
   const { rows } = await
   connection.query(`
-    SELECT events.*, organization.name
+    SELECT events.*, organization.name AS organizer
     FROM events INNER JOIN organization
     ON events.organizer_id = organization.id
     WHERE events.expire_date >= $1 
