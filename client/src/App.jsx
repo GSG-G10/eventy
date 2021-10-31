794cfc53743e67ea18e135d26db9b9110e201d0f
umar <umars.salah@gmail.com>
2021-10-31 22:00:17 +0200

    Edit card for mobile Relates #87

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import OrganizationCard from './components/organization-card';
 
+const theme = createTheme({});
 function App() {
   return (
-    <OrganizationCard/>
+    <ThemeProvider theme={theme}>
+      <OrganizationCard/>
+    </ThemeProvider>
   );
 }
 
