232601baaea046563e1205a91f64e9c432924ac9
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-11-02 18:34:59 +0200

    add single event route to app.js Relates #86

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,28 @@
 import './App.css';
+import { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Organization from './pages/Organization';
 import SingleEventCard from './pages/Event';
 
 function App() {
+  const [event, setEvent] = useState({ eventId: 7 });
+  const [organization, setOrganization] = useState('f');
+
   return (
-    <>
-      <Organization />
-      <SingleEventCard eventId = {5} />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path={`/organizations/${organization.name}`}>
+          <Organization setEventId={setEvent} />
+        </Route>
+        <Route exact path={'/organizations'}>
+          <div>Organizations</div>
+        </Route>
+        <Route exact path={'/'}>
+          <SingleEventCard eventId={event.eventId} setOrganization={setOrganization} />
+        </Route>
+      </Switch>
+    </Router>
+
   );
 }
 
