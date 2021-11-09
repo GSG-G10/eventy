3c6963bd9764460975578b86c149bf68c9bea6b2
ahmadfarid <sabbahahmad093@gmail.com>
2021-11-09 19:10:25 +0200

    refactor event header style Relates #130

---
+++ client/src/components/Header/styles.js
@@ -0,0 +1,18 @@
+const headerContainer = {
+  flexGrow: 1,
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+};
+
+const stack = {
+  padding: '1%',
+  width: '40%',
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+};
+
+export default {
+  headerContainer,
+  stack,
+};
